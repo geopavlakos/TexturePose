e6c6021c15f8ecd5ca92c330ca4f8610edff06a2
Georgios Pavlakos <geopavlakos@gmail.com>
2019-10-30 16:18:38 -0400

    Update download links

--- fetch_data.sh
+++ fetch_data.sh
@@ -3,9 +3,9 @@
 # Script that fetches all necessary data for eval
 
 # Model constants etc.
-wget http://visiondata.cis.upenn.edu/spin/data.tar.gz && tar -xvf data.tar.gz && rm data.tar.gz
+wget http://visiondata.cis.upenn.edu/texturepose/data.tar.gz && tar -xvf data.tar.gz && rm data.tar.gz
 # List of preprocessed .npz files for each dataset
-wget http://visiondata.cis.upenn.edu/spin/dataset_extras.tar.gz && tar -xvf dataset_extras.tar.gz --directory data && rm -r dataset_extras.tar.gz
+wget http://visiondata.cis.upenn.edu/texturepose/dataset_extras.tar.gz && tar -xvf dataset_extras.tar.gz --directory data && rm -r dataset_extras.tar.gz
 # Pretrained checkpoint
 wget http://visiondata.cis.upenn.edu/texturepose/texturepose_checkpoint.pth --directory-prefix=data
 
